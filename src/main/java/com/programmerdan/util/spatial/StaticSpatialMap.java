a98a65e1c2ed6c419fe762d75149df8a0d08c7f7
Daniel Boston <programmerdan@gmail.com>
2015-05-28 22:39:04 -0400

    Moving this forward. Fleshing out configuration of the map, although I have a dual index problem looming.

--- src/main/java/com/programmerdan/util/spatial/StaticSpatialMap.java
+++ src/main/java/com/programmerdan/util/spatial/StaticSpatialMap.java
@@ -37,6 +37,21 @@ public class StaticSpatialMap<V> implements SpatialMap<Integer,V> {
 	private int power = 0;
 
 	/**
+	 * Adjustment offset: floor((cellsize / 2) ^ (power + 1))
+	 */
+	private double preOffset = 0.0; 
+
+	/**
+	 * Modulo factor: cellsize ^ (power + 1)
+	 */
+	private double moduloFactor = 0.0;
+
+	/**
+	 * Division factory: cellsize ^ power
+	 */
+	private double reductionFactor = 0.0;
+
+	/**
 	 * Fast accessor for power < 1 test.
 	 */
 	private boolean subdivide = false;
@@ -58,9 +73,15 @@ public class StaticSpatialMap<V> implements SpatialMap<Integer,V> {
 	 * Data Objects  *
 	 *               */
 
+	/**
+	 * Non spatial storage variable
+	 */
 	private HashMap<StaticSpatialMap.Key,V> nonSpatial;
 
-	private HashMap<Integer,SpatialMap<Integer, V>> Spatial;
+	/**
+	 * Spatial storage variable.
+	 */
+	private HashMap<Integer,SpatialMap<Integer, V>> spatial;
 
 	/*               *
 	 * Constructors  *
@@ -90,13 +111,16 @@ public class StaticSpatialMap<V> implements SpatialMap<Integer,V> {
 			this.power = power;
 		}
 
-		this.subdivide = power > 0;
 
 		if (capacity >= 0) {
 			this.nonSpatialCapacity = capacity;
 		}
 
-		this.fullSpatial = this.nonSpatialCapacity < 1;
+		nonSpatial = null;
+		spatial = null;
+
+		resetInternals();
+
 	}
 
 
@@ -104,16 +128,45 @@ public class StaticSpatialMap<V> implements SpatialMap<Integer,V> {
 	 *   Functions   *
 	 *               */
 
+	/**
+	 * Configures the offset, modulo, reduction, subdivision, and
+	 * fullSpatial mode variables. All internal settings.
+	 */
+	private void resetInternals() {
+		preOffset = Math.pow(Math.floor( cellsize / 2.0 ), power + 1.0);
+		moduloFactor = Math.pow( cellsize, power + 1.0 );
+		reductionFactory = Math.pow( cellsize, power );
+		subdivide = power > 0;
+		fullSpatial = nonSpatialCapacity < 1;
+
+		if (fullSpatial) {
+			if (spatial == null) {
+				spatial = new HashMap<Integer, SpatialMap<Integer, V>>();
+			}
+		} else {
+			if (nonSpatial == null) {
+				nonSpatial = new HashMap<StaticSpatialMap.Key, V>();
+			}
+		}
+	}
+
+	public V put(Integer x, Integer y, Integer z, V value) {
+		
+	}
+
 	/*               *
 	 *  Subclasses   *
 	 *               */
 
+	/**
+	 * Key is a nice object representation of the natural indices
+	 */
 	public static class Key implements SpatialMap.Key<Integer>{
 		private Integer x;
 		private Integer y;
 		private Integer z;
 
-		private Integer r;
+		private Integer i;
 
 		public Integer getX() {
 			return x;
@@ -127,19 +180,38 @@ public class StaticSpatialMap<V> implements SpatialMap<Integer,V> {
 			return z;
 		}
 
-		public Integer getR() {
-			return r;
+		public Integer getIdx() {
+			return i;
 		}
 
-		public Key(Integer x, Integer y, Integer z, Integer r) {
+		public Key(Integer x, Integer y, Integer z, Integer idx) {
 			this.x = x;
 			this.y = y;
 			this.z = z;
-			this.r = r;
+			this.i = idx;
 		}
 
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof Key) {
+				Key objk = (Key) obj;
+				if (this.i == objk.getIdx() || (this.x == objk.getX() && 
+					this.y == objk.getY() && this.z == objk.getZ() ) ) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		@Override
+		public int hashCode() {
+			return i;
+		}
 	}
 
+	/**
+	 * Entry for this map
+	 */
 	public static class Entry<V> implements SpatialMap.Entry<Integer, V> {
 		private Key<Integer> key;
 		private V value;
